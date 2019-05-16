e30d7bb3df1329471cdd9e369652b7d8630c1b61
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-16 15:27:32 +0500

    fix code style

--- Vostok.ServiceDiscovery/ServiceLocatorSettings.cs
+++ Vostok.ServiceDiscovery/ServiceLocatorSettings.cs
@@ -11,6 +11,7 @@ namespace Vostok.ServiceDiscovery
     public class ServiceLocatorSettings
     {
         public int MaximumEnvironmentsDepth = 5;
+
         public string ZooKeeperNodesPrefix { get; set; } = "/service-discovery/v2";
 
         public TimeSpan IterationPeriod { get; set; } = 5.Seconds();
