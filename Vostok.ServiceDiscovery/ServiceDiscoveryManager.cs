981dfe87278ea04ebd6edaf8d13456bc816c37cb
deniaa <deniaa@skbkontur.ru>
2019-08-13 19:12:48 +0500

    Add first realization of ServiceDiscoveryManager.

---
+++ Vostok.ServiceDiscovery/ServiceDiscoveryManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Logging.Abstractions;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.ServiceDiscovery.Helpers;
+using Vostok.ServiceDiscovery.Serializers;
+using Vostok.ZooKeeper.Client.Abstractions;
+using Vostok.ZooKeeper.Client.Abstractions.Model.Request;
+
+namespace Vostok.ServiceDiscovery
+{
+    [PublicAPI]
+    public class ServiceDiscoveryManager : IServiceDiscoveryManager
+    {
+        private readonly IZooKeeperClient zooKeeperClient;
+        private readonly ServiceDiscoveryManagerSettings settings;
+        private readonly ILog log;
+
+        private readonly ServiceDiscoveryPathHelper pathHelper;
+
+        public ServiceDiscoveryManager(
+            [NotNull] IZooKeeperClient zooKeeperClient,
+            [CanBeNull] ServiceDiscoveryManagerSettings settings = null,
+            [CanBeNull] ILog log = null)
+        {
+            this.zooKeeperClient = zooKeeperClient;
+            this.settings = settings ?? new ServiceDiscoveryManagerSettings();
+            this.log = (log ?? LogProvider.Get()).ForContext<ServiceDiscoveryManager>();
+
+            pathHelper = new ServiceDiscoveryPathHelper(this.settings.ZooKeeperNodesPrefix, this.settings.ZooKeeperNodesPathEscaper);
+        }
+
+        public async Task<IReadOnlyList<string>> GetAllEnvironmentsAsync()
+        {
+            var data = await zooKeeperClient.GetChildrenAsync(new GetChildrenRequest(settings.ZooKeeperNodesPrefix));
+            data.EnsureSuccess();
+            return data.ChildrenNames;
+        }
+
+        public async Task<IReadOnlyList<string>> GetAllApplicationsAsync(string environment)
+        {
+            var data = await zooKeeperClient.GetChildrenAsync(new GetChildrenRequest(pathHelper.BuildEnvironmentPath(environment)));
+            data.EnsureSuccess();
+            return data.ChildrenNames;
+        }
+
+        public async Task<string> GetParentZoneAsync(string environment)
+        {
+            var data = await zooKeeperClient.GetDataAsync(new GetDataRequest(pathHelper.BuildEnvironmentPath(environment)));
+            data.EnsureSuccess();
+            var envData = EnvironmentNodeDataSerializer.Deserialize(data.Data);
+            return envData.ParentEnvironment;
+        }
+    }
+}
---
+++ Vostok.ServiceDiscovery/ServiceDiscoveryManagerSettings.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Helpers;
+
+namespace Vostok.ServiceDiscovery
+{
+    /// <summary>
+    /// Represents <see cref="ServiceLocator"/> settings.
+    /// </summary>
+    [PublicAPI]
+    public class ServiceDiscoveryManagerSettings
+    {
+        public string ZooKeeperNodesPrefix { get; set; } = "/service-discovery/v2";
+
+        public IZooKeeperPathEscaper ZooKeeperNodesPathEscaper { get; set; } = ZooKeeperPathEscaper.Instance;
+    }
+}
