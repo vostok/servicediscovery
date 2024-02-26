4a90e1a8d0c204499e96aa840fa1f97d8311ff8d
gladysheva <gladysheva@skbkontur.ru>
2024-02-26 10:27:55 +0500

    Separate class for envStorages tests with observeNonExistentNodes flag

--- Vostok.ServiceDiscovery.Tests/ServiceLocatorStorage/EnvironmentsStorage_Tests.cs
+++ Vostok.ServiceDiscovery.Tests/ServiceLocatorStorage/EnvironmentsStorage_Tests.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using FluentAssertions;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
-using Vostok.Commons.Testing;
 using Vostok.ServiceDiscovery.Abstractions.Models;
-using Vostok.ServiceDiscovery.ServiceLocatorStorage;
 using Vostok.ZooKeeper.Client.Abstractions;
 
 namespace Vostok.ServiceDiscovery.Tests.ServiceLocatorStorage
 {
     [TestFixture]
-    internal class EnvironmentsStorage_Tests : TestsBase
+    internal class EnvironmentsStorage_Tests : EnvironmentStorage_TestsBase
     {
         [Test]
         public void Should_track_environment_parent_with_properties()
@@ -152,80 +148,6 @@ namespace Vostok.ServiceDiscovery.Tests.ServiceLocatorStorage
                     ShouldReturnImmediately(storage, "default", expected);
                 }
             }
-        }
-
-        [Test]
-        public void Should_not_delete_environment_from_cache_if_node_was_deleted_when_observation_of_deleted_apps_is_enabled()
-        {
-            using (var storage = GetEnvironmentsStorage(observeNonExistentEnvironment: true))
-            {
-                CreateEnvironmentNode("default", "parent");
-
-                var expectedInfo = new EnvironmentInfo("default", "parent", null);
-                storage.Get("default").Should().BeEquivalentTo(expectedInfo);
-
-                DeleteEnvironmentNode("default");
-                storage.UpdateAll();
-                storage.Contains("default").Should().BeTrue();
-                storage.Get("default").Should().BeNull();
-
-                CreateEnvironmentNode("default", "parent");
-                ShouldReturn(storage, "default", expectedInfo);
-            }
-        }
-
-        [Test]
-        public void Should_not_delete_environment_from_cache_when_observation_of_deleted_apps_is_disabled_and_client_disconnected()
-        {
-            using (var storage = GetEnvironmentsStorage(observeNonExistentEnvironment: false))
-            {
-                CreateEnvironmentNode("default", "parent");
-
-                var expectedInfo = new EnvironmentInfo("default", "parent", null);
-                ShouldReturnImmediately(storage, "default", expectedInfo);
-
-                Ensemble.Stop();
-
-                storage.UpdateAll();
-                storage.Contains("default").Should().BeTrue();
-                ShouldReturnImmediately(storage, "default", expectedInfo);
-            }
-        }
-
-        [Test]
-        public void Should_delete_environment_from_cache_if_node_was_deleted_when_observation_of_deleted_apps_is_disabled()
-        {
-            var expectedInfo = new EnvironmentInfo("default", "parent", null);
-
-            using (var storage = GetEnvironmentsStorage(observeNonExistentEnvironment: false))
-            {
-                for (var i = 0; i < 10; i++)
-                {
-                    CreateEnvironmentNode("default", "parent");
-
-                    ShouldReturnImmediately(storage, "default", expectedInfo);
-
-                    DeleteEnvironmentNode("default");
-                    storage.UpdateAll();
-                    storage.Contains("default").Should().BeFalse();
-                }
-            }
-        }
-
-        private static void ShouldReturn(EnvironmentsStorage storage, string name, EnvironmentInfo info)
-        {
-            Action assertion = () => { ShouldReturnImmediately(storage, name, info); };
-            assertion.ShouldPassIn(DefaultTimeout);
-        }
-
-        private static void ShouldReturnImmediately(EnvironmentsStorage storage, string name, EnvironmentInfo info)
-        {
-            storage.Get(name).Should().BeEquivalentTo(info);
-        }
-
-        private EnvironmentsStorage GetEnvironmentsStorage(bool observeNonExistentEnvironment = true)
-        {
-            return new EnvironmentsStorage(ZooKeeperClient, PathHelper, EventsQueue, observeNonExistentEnvironment, Log);
         }
     }
 }
